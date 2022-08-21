bb522b942d45de5fb6090419fd7a18fb4101b376
virkumar <virkumar@Viresh-LP>
2022-08-21 15:00:07 +0530

    modified

--- src/main/java/MercFirstDemo.java
+++ src/main/java/MercFirstDemo.java
@@ -21,7 +21,7 @@ public class MercFirstDemo extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("MercFirstDemo for Java!\n");
+		resp.getWriter().print("Modified MercFirstDemo for Java!\n");
 	}
 
 	public static void main(String[] args) throws Exception {
